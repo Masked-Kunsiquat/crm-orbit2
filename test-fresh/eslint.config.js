e3fca445e0e199cdc140eed0b43b41a408f37240
Masked-Kunsiquat <blaineb097@gmail.com>
2025-11-19 00:26:44 -0500

    setting up pretttier/lint

---
+++ test-fresh/eslint.config.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig, globalIgnores } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = defineConfig([
+  globalIgnores(['dist/*']),
+  expoConfig,
+  {
+    files: ['babel.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist/*'],
+  },
+]);
