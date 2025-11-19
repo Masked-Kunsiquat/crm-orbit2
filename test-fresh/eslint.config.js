d9d2bee91b61c3c550182f3422dbcb5d29c68a28
Masked-Kunsiquat <blaineb097@gmail.com>
2025-11-19 00:32:01 -0500

    fix `eslint` config

--- test-fresh/eslint.config.js
+++ test-fresh/eslint.config.js
@@ -1,6 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
+const globals = require("globals");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
